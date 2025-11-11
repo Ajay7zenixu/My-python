ed0af415dedbd00545b438c670d3b26d7271acd1
Ajay7zenixu <aajaykumarr549@gmail.com>
2025-11-11 14:51:54 +0530

    Create Divisibility Test.py

---
+++ Divisibility Test.py
@@ -0,0 +1,7 @@
+Divisibility Test
+#divisibility Test
+number=int(input("enter number"))
+if number%3==0 and number%5==0:
+  print("divisible by 3and 5")
+else:
+  print("not divisible by 3 and 5 ")
