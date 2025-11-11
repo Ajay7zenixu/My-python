5baff7f626594816a09a27c228d1ee21e33b85b1
Ajay7zenixu <aajaykumarr549@gmail.com>
2025-11-11 14:21:34 +0530

    Create Grade Calculation.py

---
+++ Grade Calculation.py
@@ -0,0 +1,13 @@
+Grade Calculation
+#marks
+mark= float(input("enter mark: "))
+if mark>= 90:
+ print("excellent")
+elif mark>=80:
+ print("good")
+elif mark>=50:
+ print("average")
+elif mark>=34:
+ print("jaust pass ")
+else:
+ print("fail better luck next time")
