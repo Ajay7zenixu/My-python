a804ab09bc0497df8c0bcdac2462fc4b96d5d6f7
Ajay7zenixu <aajaykumarr549@gmail.com>
2025-11-11 14:17:20 +0530

    Create Largest of Three Numbers.py

---
+++ Largest of Three Numbers.py
@@ -0,0 +1,10 @@
+Largest of Three Numbers
+num1 =float(input("enter number num1"))
+num2 = float(input("enter numbernum2"))
+num3 = float(input("enter number num3"))
+if num1>num2 and num>3:
+  print("largest number is num1")
+elif num2>num1 and num2>num3:
+  print("largest number num2")
+else:
+  print("largest number num3")
