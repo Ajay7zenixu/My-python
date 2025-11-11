82167f97c57d9818f3aee978845bb6482ce1f7ef
Ajay7zenixu <aajaykumarr549@gmail.com>
2025-11-11 14:15:59 +0530

    Create Largest of Two Numbers.py

---
+++ Largest of Two Numbers.py
@@ -0,0 +1,9 @@
+Largest of Two Numbers
+num1=int(input("enter number num1"))
+num2=int(input("enter number num2")) 
+if num1>num2:
+  print("its a largest number")
+elif num2>num1:
+  print("its a smallest number")
+else:
+   print("its a equal number")
